b37daad7d59a1f7ea5c15a96d799f6fb72159d38
barakadewise <barakadewise@gmail.com>
2024-03-06 20:29:56 +0300

    Added api folder to front end deleted home.txt file

---
+++ frontend/venv/Api/api.py
@@ -0,0 +1,7 @@
+class ApiService:
+    Baseurl ='https://c4f9-197-250-198-16.ngrok-free.app/graphql'
+    def __init__(self,url) -> None:
+        self.url =url
+
+    def getApiResponse():
+        return 
