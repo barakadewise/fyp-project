9ddf68925688a9d315defee788e4f422f18067a8
Baraka <barakadewise@gmail.com>
2024-06-10 18:07:34 +0300

    update gqlguards to check session expiration

--- backend/fyp-api/src/auth/guard/gql-auth.guard.ts
+++ backend/fyp-api/src/auth/guard/gql-auth.guard.ts
@@ -1,16 +1,29 @@
-import { Injectable, ExecutionContext } from '@nestjs/common';
+
+import { AuthenticationError } from '@nestjs/apollo';
+import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 
 
+
 @Injectable()
 export class GqlAuthGuard extends AuthGuard('jwt') {
-  getRequest(context: ExecutionContext) {
-    const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req;
-  }
+    canActivate(context: ExecutionContext) {
+        const ctx = GqlExecutionContext.create(context);
+        const { req } = ctx.getContext();
+        return super.canActivate(new ExecutionContextHost([req]));
+    }
+
+    handleRequest(err: any, user: any) {
+        if (err || !user) {
+            throw (
+                err || new UnauthorizedException('Session expired! Please login.')
+            );
+        }
+        return user;
+    }
 }
 
 
