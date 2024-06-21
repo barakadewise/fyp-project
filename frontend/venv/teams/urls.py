8fb73273e35528aa910bc6c86846e0e0d8c38898
Baraka <barakadewise@gmail.com>
2024-06-21 14:14:07 +0300

    teams profile and views

---
+++ frontend/venv/teams/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+from .import views
+
+urlpatterns=[
+ path('dashbaord',views.getTeamDashboard,name='teamDashboard'),
+ path('youth',views.viewYouth,name='teamViewYouth'),
+ path('session',views.viewSession,name='teamSession')
+]
--- frontend/venv/teams/views.py
+++ frontend/venv/teams/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def getTeamDashboard(request):
+    return render(request,'team-dashboard.html')
+
+def viewYouth(request):
+    return render(request,'team-veiwYouth.html')
+
+def viewSession(request):
+    return render(request,'team-session.html')
