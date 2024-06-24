7de700f015d778f4b7bc01f941bd18ff5d3b599c
Baraka <barakadewise@gmail.com>
2024-06-24 20:45:03 +0300

    edit ,delete and create session for teams

--- frontend/venv/teams/urls.py
+++ frontend/venv/teams/urls.py
@@ -4,5 +4,7 @@ from .import views
 urlpatterns=[
  path('dashbaord',views.getTeamDashboard,name='teamDashboard'),
  path('youth',views.viewYouth,name='teamViewYouth'),
- path('session',views.viewSession,name='teamSession')
+ path('session',views.viewSession,name='teamSession'),
+ path('createSession',views.createTrainingSession,name="createTrainingSession"),
+ path('deleteSession',views.deleteTrainingSession,name="deleteSessionById"),
 ]
--- frontend/venv/teams/views.py
+++ frontend/venv/teams/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 
+from Api.api import ApiService
+
+#Api instance
+api_service = ApiService()
+
+#retrieve token from the session
+def getToken(request):
+    if 'token' in request.session['User']:
+        return request.session['User']['token']
+    return None
 
 def getTeamDashboard(request):
     return render(request,'team-dashboard.html')
@@ -8,4 +19,86 @@ def viewYouth(request):
     return render(request,'team-veiwYouth.html')
 
 def viewSession(request):
-    return render(request,'team-session.html')
+    print(request.session['User']['token'])
+    query= '''
+    query {
+    getTeamsTraining {
+    id
+    session
+    description
+    startDate
+    duration
+    endDate
+    noOfparticipants
+     }
+    }
+
+    '''
+
+    response = api_service.performQuery(query,api_service.getCsrfToken(request),getToken(request))
+    return render(request,'team-session.html', {"sessions":response['data']['getTeamsTraining']})
+
+def createTrainingSession(request):
+    #define the mutation 
+    mutation ='''
+    mutation CreateTraining($input: CreateTrainingInput!) {
+        createTraining(createTrainingInput: $input) {
+            id
+            }
+        }
+
+       '''
+    if request.method == "POST":
+        session = request.POST.get('session')
+        duration = request.POST.get('duration')
+        startDate = request.POST.get('startDate')
+        endDate = request.POST.get('endDate')
+        description = request.POST.get('description')
+        noOfParticipants = request.POST.get('noOfParticipants')
+
+        variables= {
+        "input": {
+        "session": session,
+        "description": description,
+        "duration": duration,
+        "startDate":startDate,
+        "endDate":endDate,
+        "noOfparticipants": int(noOfParticipants)
+         }
+        }
+
+
+        try:
+            response = api_service.performMuttion(mutation,variables,getToken(request))
+            if 'errors' in response:
+                print("errors:",response['errors'])
+                messages.error(request,response['errors'][0])
+                return redirect('teamSession')
+
+            messages.success(request,"Successfully created!")
+            return redirect('teamSession')
+          
+        except Exception as e:
+            print("exception throw",response['errors'])
+            messages.error(request,"Something Went Wrong!")
+            return redirect('teamSession')
+
+
+def deleteTrainingSession(request):
+    mutation='''
+        mutation($id:Float!) {
+         removeTraining(id: $id) {
+         message
+         statusCode
+         }
+         }
+       '''
+    if request.method=="POST":
+        id =int(request.POST.get('id'))
+        response = api_service.performMuttion(mutation,{"id":id},getToken(request))
+        if 'errors' in response:
+            messages.error(request,response['errors'])
+            return redirect('teamSession')
+        
+        messages.success(request,"SuccessFully Deleted.")
+        return redirect('teamSession')
